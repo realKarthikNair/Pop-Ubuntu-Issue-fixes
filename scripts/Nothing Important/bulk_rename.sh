8ededc23dca36b8373fa1eae3588c327d453d361
Karthik Nair <knairothera@gmail.com>
2022-04-13 00:55:08 +0530

    Update bulk_rename.sh

--- scripts/Nothing Important/bulk_rename.sh
+++ scripts/Nothing Important/bulk_rename.sh
@@ -1,2 +1,2 @@
-# actually i use this to purge all disabled ppas
+# actually i use this to enable all disabled ppas
 find -type f -name '*.save' -print0 | while read -d $'\0' f; do mv "$f" "${f%.save}"; done
