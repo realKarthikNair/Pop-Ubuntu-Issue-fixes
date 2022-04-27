9286ff25f417a61d7f0429c68e67678b0170e5df
Karthik Nair <knairothera@gmail.com>
2022-04-28 00:20:41 +0530

    Update new_doc_enable.sh

--- scripts/scripts/new_doc_enable.sh
+++ scripts/scripts/new_doc_enable.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 cd ~/Templates
-touch new
+touch TextFile.txt
+touch WordFile.txt
+touch OpenOfficeText.odt
+touch ExcelFile.xlsx
 echo
 echo Added \"New Document\" option in File Manager successfully
